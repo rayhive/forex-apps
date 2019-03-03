8e207b3988326c8bf88227c5a02a87d5472aff24
Reuben Amoako-Atta <yawatta@gmail.com>
2019-03-03 21:04:16 +0800

    Skeletal component for encapsulating forex input values

---
+++ forex-app/src/components/ForexInput.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class ForexInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = { inputValue: '' };
+    }
+
+    handleChange(e) {
+        this.props.onInputChange(e);
+        this.setState({ inputValue: e.target.value });
+    }
+
+    render() {
+        const inputLabel = this.props.label;
+        const nameAttributeValue = this.props.name;
+        const typeAttributeValue = this.props.type;
+        const inputValue = this.state.inputValue;
+
+        return (
+            <fieldset>
+                <legend>{inputLabel}:</legend>
+                <input
+                    name={nameAttributeValue}
+                    type={typeAttributeValue}
+                    value={inputValue}
+                    onChange={this.handleChange} />
+            </fieldset>
+        );
+    }
+}
+
+export default ForexInput;
